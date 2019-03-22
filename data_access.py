66de48792be8954ce525e109a1c77d56edf42c61
Jing Li <lyovse@gmail.com>
2019-03-22 16:23:19 +1100

    Add log data method, class constant variables and destructor

--- data_access.py
+++ data_access.py
@@ -3,11 +3,25 @@ import sqlite3 as lite
 
 class DataAccess:
 
+    SENSOR_DATA_TB = 'SENSOR_DATA'
+    NOTIFICATION_STATUS_TB = 'NOTIFICATION_STATUS'
+
     def __init__(self):
         self.con = lite.connect('greenhouse_monitor.db')
         with self.con:
             cur = self.con.cursor()
             cur.execute("CREATE TABLE IF NOT EXISTS "
-                        "SENSOR_DATA(timestamp DATETIME, temp NUMERIC, humid NUMERIC)")
+                        "? (timestamp DATETIME, temp NUMERIC, humid NUMERIC)",
+                        (DataAccess.SENSOR_DATA_TB,))
             cur.execute("CREATE TABLE IF NOT EXISTS "
-                        "NOTIFICATION_STATUS(date DATE, sent BOOLEAN)")
+                        "? (date DATE, sent BOOLEAN)",
+                        (DataAccess.NOTIFICATION_STATUS_TB,))
+
+    def __del__(self):
+        self.con.close()
+
+    def log_data(self, temp, humid):
+        cur = self.con.cursor()
+        cur.execute("INSERT INTO ? VALUES (DATETIME('now'), ?, ?)",
+                    (DataAccess.SENSOR_DATA_TB,), (temp,), (humid,))
+        self.con.commit()
