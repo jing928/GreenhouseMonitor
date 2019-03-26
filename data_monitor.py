4e29f5cc34668aca1dde6d1a76d4a6608e984c58
Jing Li <lyovse@gmail.com>
2019-03-26 20:14:42 +1100

    Create initial data monitor class as a controller

---
+++ data_monitor.py
@@ -0,0 +1,17 @@
+from data_access import DataAccess
+from data_collector import DataCollector
+
+
+class DataMonitor:
+
+    def __init__(self):
+        self.__data_collector = DataCollector()
+        self.__dao = DataAccess()
+
+    def start_monitor(self):
+        sensor_data = self.__data_collector.collect_data()
+        if sensor_data is not None:
+            self.__save_data(sensor_data)
+
+    def __save_data(self, data):
+        self.__dao.log_data(data['temperature'], data['humidity'])
