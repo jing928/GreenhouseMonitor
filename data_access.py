cc40b943890911a845d9a4b589f9acd80b2798bd
Jing Li <lyovse@gmail.com>
2019-03-26 13:44:17 +1100

    Fix an error where table name cannot be parameterized

--- data_access.py
+++ data_access.py
@@ -4,19 +4,13 @@ from datetime import datetime, date
 
 class DataAccess:
 
-    SENSOR_DATA_TB = 'SENSOR_DATA'
-    NOTIFICATION_STATUS_TB = 'NOTIFICATION_STATUS'
-
     def __init__(self):
         self.con = lite.connect('greenhouse_monitor.db')
         with self.con:
             cur = self.con.cursor()
-            cur.execute("CREATE TABLE IF NOT EXISTS "
-                        "? (timestamp DATETIME, temp NUMERIC, humid NUMERIC)",
-                        (DataAccess.SENSOR_DATA_TB,))
-            cur.execute("CREATE TABLE IF NOT EXISTS "
-                        "? (date DATE, sent BOOLEAN)",
-                        (DataAccess.NOTIFICATION_STATUS_TB,))
+            cur.execute("CREATE TABLE IF NOT EXISTS SENSOR_DATA"
+                        "(timestamp DATETIME, temp NUMERIC, humid NUMERIC)")
+            cur.execute("CREATE TABLE IF NOT EXISTS NOTIFICATION_STATUS (date DATE, sent BOOLEAN)")
 
     def __del__(self):
         self.con.close()
@@ -24,12 +18,10 @@ class DataAccess:
     def log_data(self, temp, humid):
         now = datetime.utcnow()
         cur = self.con.cursor()
-        cur.execute("INSERT INTO ? VALUES (?, ?, ?)",
-                    (DataAccess.SENSOR_DATA_TB,), (now,), (temp,), (humid,))
+        cur.execute("INSERT INTO SENSOR_DATA VALUES (?, ?, ?)", (now,), (temp,), (humid,))
         self.con.commit()
 
     def log_notification(self):
         today = date.today()
         cur = self.con.cursor()
-        cur.execute("INSERT INTO ? VALUES (?, ?)",
-                    (DataAccess.NOTIFICATION_STATUS_TB,), (today,), (True,))
+        cur.execute("INSERT INTO NOTIFICATION_STATUS VALUES (?, ?)", (today,), (True,))
