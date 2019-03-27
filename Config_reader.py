3e089a0939493d4b4eb0c49670da52a35f2a4a8b
darrenHub <s3641239@student.rmit.edu.au>
2019-03-27 14:38:38 +1100

    change the position of try-exception

--- Config_reader.py
+++ Config_reader.py
@@ -1,19 +1,20 @@
 import json
-from pathlib import Path
+# from pathlib import Path
 
 
 class ConfigReader:
 
-    try:
         @staticmethod
-        def read_file(path):     # the parameter is a path
-            #path = Path('GreenhouseMonitor', 'config.json')
-            readfile = open(path, "r")    # do not write path, that should be a parameter
-            loadfile = json.load(readfile)
-            return loadfile
-            # print(f.read())
-    except ValueError:
-        print("The path is not existed!")
+        def read_file(path):
+            try:
+                readfile = open(path, "r")
+            except ValueError:
+                print("The path is not existed!")
+            finally:
+                loadfile = json.load(readfile)
+                return loadfile
 
 
-#ConfigReader.read_file()
+
+
+# ConfigReader.read_file()
