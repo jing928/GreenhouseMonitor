51ae77489f9f60ee3ab937cc84867927a89d0a81
Jing Li <lyovse@gmail.com>
2019-03-27 15:47:54 +1100

    Create a method to send out of range notification

--- notifier.py
+++ notifier.py
@@ -1,5 +1,7 @@
 import json
 import requests
+from data_access import DataAccess
+from utils.enums import SensorDataCol
 
 
 class Notifier:
@@ -20,3 +22,17 @@ class Notifier:
             print('Oops...Something went wrong...')
         else:
             print('Success! Notification sent!')
+
+    def notify_out_of_range_reading(self, reading, temp_verified_result, humid_verified_result):
+        dao = DataAccess()
+        notification_sent_today = dao.get_notification_status()
+        if not notification_sent_today:
+            title = 'Attention! Reading Out Of Range!'
+            temp_info = temp_verified_result[1]
+            humid_info = humid_verified_result[1]
+            collected_time = reading[SensorDataCol.COLLECTED_AT]
+            body = "Time: {time} Temperature: {temp} *C Humidity: {humid}%\n{temp_info}" \
+                   "\n{humid_info}".format(time=collected_time, temp=reading[SensorDataCol.TEMP],
+                                           humid=reading[SensorDataCol.HUMID], temp_info=temp_info,
+                                           humid_info=humid_info)
+            self.send_notification(title, body)
