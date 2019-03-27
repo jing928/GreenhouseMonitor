3978a44b7ad8dcfc6e1d2735dc8920ab0e200ed1
darrenHub <s3641239@student.rmit.edu.au>
2019-03-27 14:07:21 +1100

    add path parameter and try exception and return value

--- Config_reader.py
+++ Config_reader.py
@@ -1,13 +1,19 @@
 import json
+from pathlib import Path
 
 
 class ConfigReader:
-    @staticmethod
-    def read_file():     # the parameter is a path
-        f = open("../GreenhouseMonitor/config.json", "r")    # do not write path, that should be a parameter
-        y = json.load(f)
-        print y
-        # print(f.read())
+
+    try:
+        @staticmethod
+        def read_file(path):     # the parameter is a path
+            #path = Path('GreenhouseMonitor', 'config.json')
+            readfile = open(path, "r")    # do not write path, that should be a parameter
+            loadfile = json.load(readfile)
+            return loadfile
+            # print(f.read())
+    except ValueError:
+        print("The path is not existed!")
 
 
-ConfigReader.read_file()
+#ConfigReader.read_file()
