c0615b3a6d09eea3e0544c915249b2357ff23a9a
Jing Li <lyovse@gmail.com>
2019-04-03 01:22:07 +1100

    Create initial report generator

---
+++ utils/report_generator.py
@@ -0,0 +1,38 @@
+from utils.data_access import DataAccess
+from utils.file_access import FileAccess
+from utils.validator import Validator
+
+
+class ReportGenerator:
+
+    def __init__(self):
+        self.__dao = DataAccess()
+        self.__report_name = ''
+        self.__report_data = [['Date', 'Status']]
+
+    def generate(self):
+        days = self.__dao.get_distinct_local_days()
+        if not days:
+            print('Error: No data in the database.')
+            return
+        sensor_data_range = FileAccess.get_sensor_data_range()
+        if sensor_data_range is None:
+            print('Error: No range config available.')
+            return
+        for day in days:
+            self.__process_day(day, sensor_data_range)
+            FileAccess.write_to_csv(self.__report_data, 'output.csv')
+
+    def __process_day(self, day, data_range):
+        min_temp = self.__dao.get_min_temp_of_day(day)
+        max_temp = self.__dao.get_max_temp_of_day(day)
+        min_humid = self.__dao.get_min_humid_of_day(day)
+        max_humid = self.__dao.get_max_humid_of_day(day)
+        day_status = Validator.verify_temp_humid_of_day(min_temp, max_temp,
+                                                        min_humid, max_humid, data_range)
+        day_row = [day, day_status]
+        self.__report_data.append(day_row)
+
+
+
+
