8da1f469844c54a12ece7572faff43b45513c621
Jing Li <lyovse@gmail.com>
2019-04-03 17:30:36 +1100

    Add main analytics class

--- analytics.py
+++ analytics.py
@@ -1,26 +1,13 @@
-import sqlite3 as lite
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
+from utils.figure_generator import FigureGenerator
 
 
-class FigureGenerator:
+class Analytics:
+
     @staticmethod
-    def generate():
-        conn = lite.connect('/home/pi/Workspaces/GreenhouseMonitor/greenhouse_monitor.db',
-                            detect_types=lite.PARSE_DECLTYPES | lite.PARSE_COLNAMES)
-
-        sql_cmd = "SELECT collected_at,temp FROM SENSOR_DATA"
-        df = pd.read_sql(sql=sql_cmd, con=conn)
-        fig, ax = plt.subplot(figsize=(8, 5), dpi=80)
-        ax.scatter(df["collected_at"], df["temp"], s=75, c="red", alpha=0.5)   # x is time, y is temperature
-        # ax.scatter(df["collected_at"], df["humid"])  # x is time, y is humid
-
-        ax.set_xlabel("collected_at", fontsize=13)
-        ax.set_ylabel("temp", fontsize=13)
-        ax.set_title("time and temp change")
-        ax.legend()
-        ax.show()
+    def run():
+        generator = FigureGenerator()
+        generator.generate_line_chart()
 
 
-FigureGenerator.generate()
+if __name__ == '__main__':
+     Analytics.run()
